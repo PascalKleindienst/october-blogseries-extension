f0ba8726439cdb75fc3eebf6562cd716ef8b0bf8
Pascal Kleindienst <mail@pascalkleindienst.de>
2015-07-14 15:55:02 +0200

    added Series Model

---
+++ models/Series.php
@@ -0,0 +1,37 @@
+<?php namespace PKleindienst\BlogSeries\Models;
+
+use Model;
+
+/**
+ *  Series Model
+ * @package PKleindienst\BlogSeries\Models
+ */
+class Series extends Model
+{
+    /**
+     * @var string The database table used by the model.
+     */
+    public $table = 'pkleindienst_blogseries_series';
+
+    /**
+     * @var array
+     */
+    protected $slugs = ['slug' => 'title'];
+
+    /**
+     * @var array Relations
+     */
+    public $hasMany = [
+        'posts' => [
+            'RainLab\Blog\Models\Post',
+        ],
+    ];
+
+    /**
+     * @return mixed
+     */
+    public function getPostCountAttribute()
+    {
+        return $this->posts()->count();
+    }
+}
---
+++ updates/create_series_table.php
@@ -0,0 +1,48 @@
+<?php namespace PKleindienst\BlogSeries\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+use System\Classes\PluginManager;
+
+/**
+ * Create Series Table
+ * @package PKleindienst\BlogSeries\Updates
+ */
+class CreateSeriesTable extends Migration
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function up()
+    {
+        if (PluginManager::instance()->hasPlugin('RainLab.Blog')) {
+            Schema::create('pkleindienst_blogseries_series', function ($table) {
+                $table->engine = 'InnoDB';
+                $table->increments('id');
+                $table->string('title')->unique();
+                $table->string('slug')->unique();
+                $table->string('description')->nullable();
+                $table->timestamps();
+            });
+
+            Schema::table('rainlab_blog_posts', function ($table) {
+                $table->integer('series_id')->unsigned()->nullable()->default(null);
+                $table->foreign('series_id')->references('id')->on('pkleindienst_blogseries_series')->onDelete('cascade');
+            });
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function down()
+    {
+        if (PluginManager::instance()->hasPlugin('RainLab.Blog')) {
+            Schema::table('rainlab_blog_posts', function ($table) {
+                $table->dropIndex('series_id');
+                $table->dropColumn('series_id');
+            });
+            Schema::dropIfExists('pkleindienst_blogseries_series');
+        }
+    }
+}
